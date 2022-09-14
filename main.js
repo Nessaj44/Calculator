70529eb3198e85163d8781f272f393c79a616f14
Nessaj44 <johnedmoncoc@gmail.com>
2022-09-14 10:29:25 +0800

    Remove unused declared variables

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 const numberButtons = document.querySelectorAll('.gridItem-number');
 const operatorButtons = document.querySelectorAll('.gridItem-operator');
-const clearButton = document.querySelector('.gridItem-clear');
-const deleteButton = document.querySelector('.gridItem-delete');
-const equalButton = document.querySelector('.gridItem-equal');
-const decimalButton = document.querySelector('.gridItem-decimal');
 const inputField = document.querySelector('#currentInput');
 const resultField = document.querySelector('#resultInput');
 let a = '';
 let b = '';
-let operatorValue = '';
 let operation = '';
 let c = '';
 
